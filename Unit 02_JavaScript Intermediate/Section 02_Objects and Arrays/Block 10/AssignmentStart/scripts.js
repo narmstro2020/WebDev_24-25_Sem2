34bc087d8a6e75ff84f572624b3739476126e486
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-02-03 08:12:12 -0500

    Block 10

---
+++ Unit 02_JavaScript Intermediate/Section 02_Objects and Arrays/Block 10/AssignmentStart/scripts.js
@@ -0,0 +1,14 @@
+const book = {
+    title: "Foundation",
+    // TODO: finish off with author key value pair year key value pair.
+    // TODO: like I did with title,  you can use your own favorite book.
+    getSummary: function (){
+        return `Title: ${this.title}`;  // TODO: continue with remaining properties of the book (author, year)
+
+    }
+};
+
+// TODO: add a genre key value pair.
+// TODO: delete the year property
+// TODO: console.log(book)
+
